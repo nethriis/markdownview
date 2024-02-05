9f9894a98037bb705b58df6eeef7cf9931b351b5
David <72009265+DavidAbou@users.noreply.github.com>
2024-02-05 00:47:19 -0400

    feat: seo

--- app.vue
+++ app.vue
@@ -5,9 +5,46 @@ import '@fontsource/geist-sans/600.css'
 import '@fontsource/geist-sans/700.css'
 import '@fontsource/geist-mono/400.css'
 import '@fontsource/geist-mono/700.css'
+
+useHead({
+  htmlAttrs: {
+    lang: 'en'
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'
+    }
+  ],
+  script: [
+    {
+      'data-name': 'BMC-Widget',
+      'data-cfasync': 'false',
+      src: 'https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js',
+      'data-id': 'nethris',
+      'data-description': 'Support me on Buy me a coffee!',
+      'data-message': '',
+      'data-color': '#BD5FFF',
+      'data-position': 'Right',
+      'data-x_margin': '18',
+      'data-y_margin': '18'
+    }
+  ]
+})
+
+useSeoMeta({
+  ogImage:
+    'https://github.com/nethriis/markdownview/blob/main/assets/images/og-image.png?raw=true',
+  twitterImage:
+    'https://github.com/nethriis/markdownview/blob/main/assets/images/og-image.png?raw=true',
+  twitterTitle: 'MarkdownView',
+  twitterDescription: 'A simple, fast, and free markdown viewer.'
+})
 </script>
 
 <template>
+  <NuxtLoadingIndicator color="indigo" />
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,23 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['@/assets/css/main.css'],
-  modules: ['nuxt-monaco-editor', 'nuxt-icon', '@vueuse/nuxt', '@nuxt/ui'],
+  modules: [
+    'nuxt-monaco-editor',
+    'nuxt-icon',
+    '@vueuse/nuxt',
+    '@nuxt/ui',
+    '@nuxtjs/seo'
+  ],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {}
     }
+  },
+  site: {
+    url: 'https://markdownview.com',
+    name: 'MarkdownView',
+    description: 'A simple, fast, and free markdown viewer.',
+    defaultLocale: 'en'
   }
 })
